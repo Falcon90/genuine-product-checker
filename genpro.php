7f2c1d1a18f978191dbba1de765761fa3d6e988f
Mahmoud Ofeisa <e.m.ofeisa@gmail.com>
2021-01-16 12:43:25 +0400

    Update genpro.php
    Fixing the database prefix issue

--- genpro.php
+++ genpro.php
@@ -3,11 +3,14 @@
  * Plugin Name: Genuine product checker
  * Plugin URI: 
  * Description: A solution to add your products serial number and guarantee and give the user capability to check if his product GENUINE or not.
- * Version: 1.0
+ * Version: 1.1
  * Author: Mahmoud Ofeisa
  * Author URI: www.mahmoud-ofeisa.com
  */
-
+register_activation_hook(__FILE__,'genpro_activation_function');
+add_action( 'admin_init', 'genpro_register_settings' );
+add_action('admin_menu', 'genpro_register_options_page');
+add_action('admin_menu', 'genpro_products_list');
 function genpro_activation_function() {
     global $wpdb;
     $table_name = $wpdb->prefix . 'genpro_products';
@@ -15,17 +18,15 @@ function genpro_activation_function() {
     
     $sql = "CREATE TABLE $table_name (
      id mediumint(9) NOT NULL AUTO_INCREMENT,
-     serial mediumint(255),
-     guarantee mediumint(255),
+     serial varchar(255),
+     guarantee varchar(255),
      reg_date date,
      UNIQUE KEY id (id)
     ) $charset_collate;";
-    
-   $result = $wpdb->query($sql); 
+    if(($wpdb->get_var("show tables like ".$table_name)) != $table_name) {
+        $result = $wpdb->query($sql);
+    }
 }
-
-register_activation_hook(__FILE__,'genpro_activation_function');
-
 function genpro_register_settings() {
     add_option( 'genpro_option_name', true );
     register_setting( 'genpro_options_group', 'genpro_option_name', 'genpro_callback' );
@@ -42,13 +43,9 @@ function genpro_register_settings() {
     add_option( 'genpro_container_id', 'genpro_container' );
     register_setting( 'genpro_options_group', 'genpro_container_id', 'genpro_callback' );
 }
-add_action( 'admin_init', 'genpro_register_settings' );
-
 function genpro_register_options_page() {
     add_options_page('Genpro settings', 'Genpro settings', 'manage_options', 'genpro', 'genpro_options_page');
 }
-add_action('admin_menu', 'genpro_register_options_page');
-
 function genpro_options_page(){
     wp_enqueue_script( 'genpro', plugins_url( '/js/genpro.js', __FILE__ ) );
     wp_enqueue_style( 'genpro', plugins_url( '/css/genpro.css', __FILE__ ) );
@@ -91,11 +88,10 @@ function genpro_options_page(){
             <?php  submit_button(); ?>
         </form>
         <h2>Use this shortcode in any page or post to show product checker: <span style="color:#d47500;margin-left:10px;font-weight:700;"><input type="text" class="genpro_shortcode" value="[genpro-check-serial]" readonly/></span><button onclick="CopyShortcode()">Copy shortcode</button> </h2> 
+        <div>In case you got this error: "Publishing failed. The response is not a valid JSON response." after puplishing the page, Install this plugin and use the <a href="https://wordpress.org/plugins/classic-editor/">classic editor</a></div>
     </div>
 <?php } ?>
-
 <?php
-add_action('admin_menu', 'genpro_products_list');
 function genpro_products_list() {
     $page_title = 'Products list';
     $menu_title = 'Products list';
@@ -107,13 +103,12 @@ function genpro_products_list() {
 
     add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
 }
-
 function genpro_products_list_display(){
     wp_enqueue_style( 'genpro', plugins_url( '/css/genpro.css', __FILE__ ) );
     $exist = 3;
     $delete = 0;
     global $wpdb;
-	$final_product_query =  "select * from wp_genpro_products";
+	$final_product_query =  "select * from ".$wpdb->prefix."genpro_products";
     $final_products = $wpdb->get_results($final_product_query, OBJECT);
     if ( ! isset( $_POST['nonce_submit'] ) || ! wp_verify_nonce( $_POST['nonce_submit'], 'nonce_submit_action' ) ) {
         // do nothing
@@ -121,27 +116,30 @@ function genpro_products_list_display(){
         if($_SERVER["REQUEST_METHOD"] == "POST") {
            if(isset($_POST['genpro_delete'])){
                 $genpro_serial_delete = sanitize_text_field($_POST['genpro_delete']);
-                $delete_product_query =  "DELETE from wp_genpro_products where serial = '$genpro_serial_delete'";
+                $delete_product_query =  "DELETE from ".$wpdb->prefix."genpro_products where serial = '$genpro_serial_delete'";
                 $delete_product_res = $wpdb->get_results($delete_product_query, OBJECT);
                 $delete = 1;
             }else{
-                $genpro_serial = sanitize_text_field($_POST['genpro_serial']);
-                $genpro_guarantee = sanitize_text_field($_POST['genpro_guarantee']);
-             $dt2 = new DateTime();
+                if ( isset( $_POST['genpro_serial'] ) ) {
+                    $genpro_serial = sanitize_text_field($_POST['genpro_serial']);
+                }
+                if ( isset( $_POST['genpro_guarantee'] ) ) {
+                    $genpro_guarantee = sanitize_text_field($_POST['genpro_guarantee']);
+                }
+                $dt2 = new DateTime();
                 $adding_date = $dt2->format('Y-m-d');
                 $exist = 0;
                 if(strlen($genpro_serial) >= 1){
-                foreach ( $final_products as $final_product ) {
+                    foreach ( $final_products as $final_product ) {
                         if($final_product->serial == $genpro_serial){
-                         $exist = 1;
+                            $exist = 1;
                         }
                     } 
                 }else{
                     $exist = 1;
                 }
-
                 if(!$exist){
-                    $insert_product_query =  "INSERT INTO wp_genpro_products (serial, guarantee, reg_date) VALUES ('$genpro_serial', '$genpro_guarantee', '$adding_date');";
+                    $insert_product_query =  "INSERT INTO ".$wpdb->prefix."genpro_products (serial, guarantee, reg_date) VALUES ('$genpro_serial', '$genpro_guarantee', '$adding_date');";
                     $insert_product_res = $wpdb->get_results($insert_product_query, OBJECT);
                 }
             }
@@ -154,7 +152,9 @@ function genpro_products_list_display(){
         <h3>Adding new products:</h3>
         <table>
             <tr valign="top">
-                <th><input type="text" placeholder="Serial" name="genpro_serial"/></th>
+                <th><input type="text" placeholder="Serial" name="genpro_serial"/>
+                <?php wp_nonce_field( 'nonce_submit_action', 'nonce_submit' ); ?>
+                </th>
                 <?php if(get_option('genpro_option_name')){ ?>
                     <th><input type="text" placeholder="Guarantee" name="genpro_guarantee"  />
                     <?php wp_nonce_field( 'nonce_submit_action', 'nonce_submit' ); ?>
@@ -164,9 +164,7 @@ function genpro_products_list_display(){
             </tr>
         </table>
     </form>
-
 <?php
-    
     if(!$exist){
         echo "<div class='genpro-alert success'>";
         echo "<h3>Product with serial '".$genpro_serial."' Added successfully!</h3>";
@@ -182,7 +180,6 @@ function genpro_products_list_display(){
         echo "</div>";
         $delete = 0;
     }
-    
 	echo "<table id='genpro-prodTable'>";
 	    echo "<tr class='title-row'>";
 	        echo "<th>Serial</th>";
@@ -196,28 +193,32 @@ function genpro_products_list_display(){
 	        echo "<td>$final_product->guarantee</td>";
             echo "<td>$final_product->reg_date</td>";
             echo "<td><form method=\"post\" action=\"\">
-                    <input type=\"hidden\" name=\"genpro_delete\" value=\"$final_product->serial\" />
-                    <input type=\"submit\" value=\"Delete\">
+                    <input type=\"hidden\" name=\"genpro_delete\" value=\"$final_product->serial\" />";
+                    wp_nonce_field( 'nonce_submit_action', 'nonce_submit' );
+                    echo "<input type=\"submit\" value=\"Delete\">
                 </form></td>";
 	    echo "</tr>";
 	}
     echo "</table>";
     }
 }
-
 function genpro_products_list_checkout($atts) {
     wp_enqueue_style( 'genpro', plugins_url( '/css/genpro-check.css', __FILE__ ) );
     global $wpdb;
     $genuine = 3;
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $genpro_serial = sanitize_text_field($_POST['genpro_check_serial']);
-        $genpro_guarantee = sanitize_text_field($_POST['genpro_check_guarantee']);
+        if ( isset( $_POST['genpro_check_serial'] ) ) {
+            $genpro_serial = sanitize_text_field($_POST['genpro_check_serial']);
+        }
+        if ( isset( $_POST['genpro_check_guarantee'] ) ) {
+            $genpro_guarantee = sanitize_text_field($_POST['genpro_check_guarantee']);
+        }
         $check_product_query = "";
 
         if(get_option('genpro_option_name')){
-            $check_product_query =  "select * from wp_genpro_products where serial = '$genpro_serial' && guarantee = '$genpro_guarantee'";
+            $check_product_query =  "select * from ".$wpdb->prefix."genpro_products where serial = '$genpro_serial' && guarantee = '$genpro_guarantee'";
         }else{
-            $check_product_query =  "select * from wp_genpro_products where serial = '$genpro_serial'";
+            $check_product_query =  "select * from ".$wpdb->prefix."genpro_products where serial = '$genpro_serial'";
         }    
         
         $check_product_res = $wpdb->get_results($check_product_query, OBJECT);
@@ -228,7 +229,7 @@ function genpro_products_list_checkout($atts) {
         }  
     }
     ?>
-    <div >
+    <div>
     <div id="<?php  _e(get_option('genpro_container_id'))?>">
 	    <form method="post" action="" id="genpro-check-form">
             <input type="text" class="genpro_serial" placeholder="<?php  _e(get_option('genpro_serial_text')) ?>" name="genpro_check_serial"  />
@@ -252,4 +253,3 @@ if($genuine == 1){
 ?> </div> <?php
 }
 add_shortcode('genpro-check-serial', 'genpro_products_list_checkout');
-?>
